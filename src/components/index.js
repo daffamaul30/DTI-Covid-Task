ca498c2725369fa1f0b76c0b7b66b5ee5a8b6e2d
Daffa <daffamaulana30@gmail.com>
2020-11-05 23:50:10 +0800

    update: use pagination-react-hooks

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as HeaderPage } from './headerPage';
 export { default as CoronaNews } from './coronaNews';
-export { default as Pagination } from './pagination';
 export { default as Loading } from './loading';
